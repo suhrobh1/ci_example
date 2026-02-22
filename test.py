a729a634a2eb5d0673928343553380de9d7ed74c
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2026-02-22 15:52:19 -0800

    Fix indentation of test1 method in test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import unittest
 import task
 
 class TestCase(unittest.TestCase):
-    def test1(self):
-        expected = "Hello World"
-        self.assertEqual(task.myfunc(), expected)
+    
+        def test1(self):
+            expected = "Hello World"
+            self.assertEqual(task.myfunc(), expected)
 
 
 if __name__ == '__main__':
